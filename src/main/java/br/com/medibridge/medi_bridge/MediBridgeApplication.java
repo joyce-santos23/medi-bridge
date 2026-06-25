1d644e1db84701347de5ddc0c08efbd0a0896a39
Joyce Santos <joycectba@hotmail.com>
2026-06-25 16:54:38 -0300

    first commit

---
+++ src/main/java/br/com/medibridge/medi_bridge/MediBridgeApplication.java
@@ -0,0 +1,13 @@
+package br.com.medibridge.medi_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediBridgeApplication.class, args);
+	}
+
+}
